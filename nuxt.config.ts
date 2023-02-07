9f90a75c602ddc290c3be6ebd9c7a08942f86b51
Jiri Kratochvil <jiri.kratochvil@sharry.tech>
2023-02-07 17:12:49 +0100

    Added theme forest to html

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      htmlAttrs: {
+        'data-theme': 'forest',
+      },
+    },
+  },
   modules: ['@nuxtjs/tailwindcss', 'nuxt-icon'],
   typescript: {
     shim: false,
